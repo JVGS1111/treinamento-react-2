a85e6c3fb5e6963a15ad996e5f9a1dd4681d3929
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-08-28 16:56:55 -0300

    adicionado formulario basico

--- formulario-em-react/src/App.js
+++ formulario-em-react/src/App.js
@@ -1,10 +1,17 @@
+import { Component, Fragment } from 'react';
 import './App.css';
+import FormularioCadastro from './components/formularioCadastro/formularioCadastro.jsx';
 
-function App() {
-  return (
-    <h1>Formulario de cadastro</h1>
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1> Formulario de cadastro</h1 >
+        < FormularioCadastro />
+      </Fragment>
 
-  );
+    )
+  }
 }
 
 export default App;
---
+++ formulario-em-react/src/components/formularioCadastro/formularioCadastro.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function FormularioCadastro() {
+    return (
+        <form >
+            <label>Nome</label>
+            <input type="text"></input>
+
+            <label>Sobrenome</label>
+            <input type="text"></input>
+
+            <label>CPF</label>
+            <input type="text"></input>
+
+            <label>Promocoes</label>
+            <input type="checkbox"></input>
+
+            <label>Novidades</label>
+            <input type="checkbox"></input>
+
+            <button>Cadastrar</button>
+        </form >);
+}
+
+export default FormularioCadastro;
