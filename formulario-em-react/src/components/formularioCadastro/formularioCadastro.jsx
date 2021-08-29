9eb65e657bc5db8ae3a0505dd01253566c2d8cc3
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-08-29 15:12:25 -0300

    Update formularioCadastro.jsx

--- formulario-em-react/src/components/formularioCadastro/formularioCadastro.jsx
+++ formulario-em-react/src/components/formularioCadastro/formularioCadastro.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core/';
 
 function FormularioCadastro() {
+    let nome = ""
     return (
-        <form >
-            <TextField fullWidth margin="normal" variant="outlined" id="nome" label="Nome" />
-            <TextField fullWidth margin="normal" variant="outlined" id="sobrenome" label="Sobrenome" />
-            <TextField fullWidth margin="normal" variant="outlined" id="cpf" label="CPF" />
-            <FormControlLabel label="Promocoes" control={<Switch name="promocoes" defaultChecked="true" color="primary" />} />
-            <FormControlLabel label="Novidades" control={<Switch name="Novidades" defaultChecked="true" color="primary" />} />
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            console.log(nome);
+        }}>
+            <TextField
+                onChange={
+                    (event) => {
+                        nome = event.target.value;
+                    }
+                }
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                id="nome"
+                label="Nome" />
+            <TextField
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                id="sobrenome"
+                label="Sobrenome" />
+            <TextField
+                fullWidth margin="normal"
+                variant="outlined"
+                id="cpf"
+                label="CPF" />
+            <FormControlLabel
+                label="Promocoes"
+                control={<Switch name="promocoes" defaultChecked={true} color="primary" />} />
+            <FormControlLabel
+                label="Novidades"
+                control={<Switch name="Novidades" defaultChecked={true} color="primary" />} />
 
             <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
         </form >);
